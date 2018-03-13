3ff77e8272f9f02331734c5ff231676b668f55b3
Valin Short <valin.short1998@gmail.com>
2018-03-13 16:29:27 -0400

    guessing bar, and user input

--- app/src/main/java/com/example/valindelsignore/guessinggame/GameActivity.java
+++ app/src/main/java/com/example/valindelsignore/guessinggame/GameActivity.java
@@ -3,23 +3,14 @@ package com.example.valindelsignore.guessinggame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startGame();
-
+        setContentView(R.layout.activity_game);
     }
 
 
-
-    private void startMenu(){
-        Intent playGame = new Intent(this,class);
-
-    }
 }
-
 
